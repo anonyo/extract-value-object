ca62a56aeb3665e43a49953751134e245954e451
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-08-21 16:39:11 -0400

    Not call execute on nil when negative or number more than 100 is passed as score

--- lib/score.rb
+++ lib/score.rb
@@ -10,21 +10,10 @@ class Score
     c_mark = CMark.new(score)
     b_mark = BMark.new(score)
     a_mark = AMark.new(score)
+    no_score = NoScore.new(score)
 
-    scores_list = [ failing_mark, d_mark, c_mark, b_mark, a_mark]
+    scores_list = [ failing_mark, d_mark, c_mark, b_mark, a_mark, no_score ]
     find_score = scores_list.find { |score| score.matches? }
     find_score.execute
-
- #  if fail.matches?
- #    fail.execute
- #  elsif @score < 70
- #    'D'
- #  elsif @score < 80
- #    'C'
- #  elsif @score < 90
- #    'B'
- #  else
- #    'A'
- #  end
   end
 end
