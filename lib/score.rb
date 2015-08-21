9ab90c8fecf1259b81a3c2939ff6b08648afcbb0
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-08-21 16:30:45 -0400

    Attempt to replace if statement with object

--- lib/score.rb
+++ lib/score.rb
@@ -1,19 +1,26 @@
 class Score
+  attr_reader :score
   def initialize(score)
     @score = score
   end
 
   def rating
-    if @score < 60
-      'F'
-    elsif @score < 70
-      'D'
-    elsif @score < 80
-      'C'
-    elsif @score < 90
-      'B'
-    else
-      'A'
-    end
+    fail = FailingMark.new(score)
+
+    scores_list = [ fail ]
+    find_score = scores_list.find { |score| score.matches? }
+    find_score.execute
+
+ #  if fail.matches?
+ #    fail.execute
+ #  elsif @score < 70
+ #    'D'
+ #  elsif @score < 80
+ #    'C'
+ #  elsif @score < 90
+ #    'B'
+ #  else
+ #    'A'
+ #  end
   end
 end
